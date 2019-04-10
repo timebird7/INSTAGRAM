4407a0bf95e98b9f960441ae5578c55c0b99d136
Kyumin Kim <timebird7@gmail.com>
2019-04-10 04:38:37 +0000

    modify textfield of create.html

--- posts/forms.py
+++ posts/forms.py
@@ -2,17 +2,18 @@ from django import forms
 from .models import Post
 
 class PostModelForm(forms.ModelForm):
-    content = forms.CharField(label="content")
+    content = forms.CharField(
+        label="content", 
+        widget=forms.Textarea(attrs={
+                'rows': 5,
+                'cols': 50,
+                'placeholder': '지금 뭘 하고 계신가요?',
+            }
+        )
+    )
     
     class Meta:
         model = Post
         # input을 만들 칼럼 값을 list로 만들어 넣어줌.
         fields = ['content',]
-        widget = {
-            'content': forms.Textarea(attrs={
-                'class': '',
-                'rows': 5,
-                'cols': 50,
-                'placeholder': '지금 뭘 하고 계신가요?',
-            })
-        }
+        
