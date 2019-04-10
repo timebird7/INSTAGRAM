171bf3097dbee675de00b3e3a31229499253ecca
Kyumin Kim <timebird7@gmail.com>
2019-04-10 00:48:13 +0000

    posts의 create 액션 추가

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = 'posts'
 
 urlpatterns = [
-    
+    path('create/', views.create, name='create'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def create(request):
+    return render(request,'posts/create.html')
