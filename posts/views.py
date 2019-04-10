92cf4b16507a7298437ee861936faada43791816
Kyumin Kim <timebird7@gmail.com>
2019-04-10 04:55:02 +0000

    CRUD complete

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostModelForm
 from .models import Post
 
@@ -24,3 +24,19 @@ def list(request):
         'posts': posts
     }
     return render(request,'posts/list.html', context)
+    
+def delete(request,post_id):
+    post = get_object_or_404(Post,pk=post_id)
+    post.delete()
+    return redirect('posts:list')
+    
+def update(request,post_id):
+    post = get_object_or_404(Post,pk=post_id)
+    if request.method == 'POST':
+        form = PostModelForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:list')
+    else:
+        form = PostModelForm(instance=post)
+        return render(request, 'posts/create.html', {'form': form})
