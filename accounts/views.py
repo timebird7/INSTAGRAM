28f9e5c14ca9f0f3a728d3cd74c2ca42a4a9b437
Kyumin Kim <timebird7@gmail.com>
2019-04-16 08:41:41 +0000

    user profile page

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate, get_user_model
+from django.contrib import messages
 
 # Create your views here.
 def signup(request):
@@ -11,6 +12,7 @@ def signup(request):
             auth_login(request, user) 
             return redirect('posts:list')
         else:
+            messages.success(request, 'not valid')
             return redirect('accounts:signup')
     else:
         form = UserCreationForm()
@@ -26,6 +28,7 @@ def login(request):
             auth_login(request, form.get_user())
             return redirect('posts:list')
         else:
+            messages.success(request, 'not valid')
             return redirect('accounts:login')
     else:
         form = AuthenticationForm()
@@ -37,3 +40,12 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return redirect('posts:list')
+    
+def profile(request, username):
+    profile = get_object_or_404(get_user_model(), username=username)
+    posts = profile.post_set.all()
+    context = {
+        'profile': profile,
+        'posts': posts,
+    }
+    return render(request, 'accounts/profile.html', context)
